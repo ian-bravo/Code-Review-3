1f7177f637e78ee4bfd82784325da1499f9b88df
ian-bravo <bravo.ian@gmail.com>
2023-08-18 09:50:29 -0700

    WIP

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let userInput = "5"
+function subAlgo(number) {
+    let splitArr = [];
+    for (let i = 1; i <= number; i++) {
+        splitArr.push(i);
+    }
+    console.log(splitArr);
+    console.log (typeof splitArr);
+}
