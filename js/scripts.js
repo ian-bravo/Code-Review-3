8829d10e13f045e2983447199750eb300a4338ba
ian-bravo <bravo.ian@gmail.com>
2023-08-18 13:49:22 -0700

    continue to debug, refactor JS to pass test 3 and all subsequent tests

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,24 @@ function subAlgo(number) {
         
         splitArr.push(i);
     }
-    console.log(splitArr);
+    console.log(splitArr); //array of 0 to N
     const stringSplitArr = splitArr.toString();
-    console.log(stringSplitArr);
-    const stringArr = stringSplitArr.split(',');
+    console.log(stringSplitArr); //converts array into a string.
+
+    // if 2 is present in the string, replace that value with "b". else if 1 replace with "a".
+
+
+    
+    //need to change string to array.
+    const stringArr = stringSplitArr.split(','); //converts string to array with string data values
     console.log(stringArr);
+
     // stringArr.forEach(function(replaceNumber){
     //     if (replaceNumber === "1"){
     //         splitArr.push("a");
     //     }
     // });
-    splitArr.splice(1, 1, "a");
+    splitArr.splice(1, 1, "a"); //replaces the 1 index with a
     console.log(splitArr);
     // splitArr.splice(2, 2, "b");
 //let's me save the splitArr as a new array as replaceArr
