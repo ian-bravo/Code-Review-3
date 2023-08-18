83588feb4b070cdee4b7f872a26d26be44090acd
ian-bravo <bravo.ian@gmail.com>
2023-08-18 09:59:31 -0700

    pass test 1

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ let userInput = "5"
 function subAlgo(number) {
     let splitArr = [];
     for (let i = 1; i <= number; i++) {
-        splitArr.push(i);
+        splitArr.push("" + i);
     }
     console.log(splitArr);
-    console.log (typeof splitArr);
+    
 }
