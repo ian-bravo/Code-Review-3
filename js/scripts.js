40c6d61ed24e953dc4548f23b4e44d9a2ce90e48
ian-bravo <bravo.ian@gmail.com>
2023-08-18 11:02:27 -0700

    continue to debug

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
+//Business Logic
+
 let userInput = "5"
 function subAlgo(number) {
-    let splitArr = [];
-    for (let i = 1; i <= number; i++) {
-        splitArr.push("" + i);
+    const splitArr = [];
+    for (let i = 0; i <= number; i++) {
+        splitArr.push(i);
     }
     console.log(splitArr);
+    
+    const replaceArr = splitArr.splice(" ");
+    splitArr.forEach(function(replaceNumber){
+        if (replaceNumber === 1){
+            replaceArr.push("a");
+        }
+        console.log(replaceArr);
+    });
+    // return splitArr;
+    // console.log(replaceArr);
     
 }
