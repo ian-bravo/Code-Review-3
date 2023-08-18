d263d3b605cd18499541a28801bfd3172e8191e5
ian-bravo <bravo.ian@gmail.com>
2023-08-18 12:19:18 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,22 @@ function subAlgo(number) {
         splitArr.push(i);
     }
     console.log(splitArr);
+    const stringSplitArr = splitArr.toString();
+    console.log(stringSplitArr);
+    const stringArr = stringSplitArr.split(',');
+    console.log(stringArr);
+    // stringArr.forEach(function(replaceNumber){
+    //     if (replaceNumber === "1"){
+    //         splitArr.push("a");
+    //     }
+    // });
     splitArr.splice(1, 1, "a");
     console.log(splitArr);
+    // splitArr.splice(2, 2, "b");
 //let's me save the splitArr as a new array as replaceArr
     // const replaceArr = number.split("");
     // console.log(replaceArr);
-    // splitArr.forEach(function(replaceNumber){
-    //     if (replaceNumber === "1"){
-    //         replaceArr.push("a");
-    //     }
-    // });
+
     // console.log(replaceArr);
     // return splitArr;
     // console.log(replaceArr);
