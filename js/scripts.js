32d26630affeafa9e257c00dfcf89abc6fe9cd52
ian-bravo <bravo.ian@gmail.com>
2023-08-18 11:50:15 -0700

    pass test 2

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,21 @@ let userInput = "5"
 function subAlgo(number) {
     const splitArr = [];
     for (let i = 0; i <= number; i++) {
+        
         splitArr.push(i);
     }
     console.log(splitArr);
-    
-    const replaceArr = splitArr.splice(" ");
-    splitArr.forEach(function(replaceNumber){
-        if (replaceNumber === 1){
-            replaceArr.push("a");
-        }
-        console.log(replaceArr);
-    });
+    splitArr.splice(1, 1, "a");
+    console.log(splitArr);
+//let's me save the splitArr as a new array as replaceArr
+    // const replaceArr = number.split("");
+    // console.log(replaceArr);
+    // splitArr.forEach(function(replaceNumber){
+    //     if (replaceNumber === "1"){
+    //         replaceArr.push("a");
+    //     }
+    // });
+    // console.log(replaceArr);
     // return splitArr;
     // console.log(replaceArr);
     
